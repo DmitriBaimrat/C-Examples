e4ee82cc580c13295bd20c73e4f38128c43fcd97
Dmitri Baimrat <baimratus@mail.ru>
2022-09-18 15:25:41 +0500

    Example 004 Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -4,3 +4,12 @@ double numberB = new Random().Next(1, 501);
 System.Console.WriteLine(numberB);
 
 System.Console.WriteLine(numberA / numberB);
+
+// new Random().Next(min, max-1) - Генератор случайных чисел
+// - - Вычитание
+// + - Сложение
+// = - Присваивание
+// / - Деление
+// * - Умножение
+// % - Для назождения остатка от деления
+// () - Для измены приоритета
