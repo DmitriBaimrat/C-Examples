ff9bde64ec6b2772366747da3af37a902a952a75
Dmitri Baimrat <baimratus@mail.ru>
2022-09-18 15:41:30 +0500

    Example 005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    System.Console.WriteLine("Привет, " + username + "!");
+}
