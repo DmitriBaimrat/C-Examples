b2a1ebb436bc35c7ed06f89adccd956d1e4ff419
Dmitri Baimrat <baimratus@mail.ru>
2022-09-18 15:20:35 +0500

    Example 004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1, 501);
+System.Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 501);
+System.Console.WriteLine(numberB);
+
+System.Console.WriteLine(numberA / numberB);
